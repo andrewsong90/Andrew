823f53765dbe13796f44e0cddaa5c6d8ed2daf1d
Andrew Song <andrewsong90@gmail.com>
2013-06-03 23:17:51 -0400

    First Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Andrew::Application.config.secret_token = 'b7b3828fa01e9a8a2c016364b9efdb3309b13e043a093b72c4a1312129fe5c124bf91fb0347b38abf10874f3c52089a109f29620aaae096c6386bbc6da7a7de9'
